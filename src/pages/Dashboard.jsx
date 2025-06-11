c1b82eba03c5879432a234e13e4867abb7607d3a
supriyo522 <134998523+supriyo522@users.noreply.github.com>
2025-06-11 22:20:43 +0530

    Update Dashboard.jsx

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,25 +1,8 @@
-// import React from 'react';
-// import CreateTask from '../components/Task/CreateTask';
-// import TaskList from '../components/Task/TaskList';
-// import CalendarView from '../components/Task/CalendarView';
-
-// const Dashboard = () => {
-//   return (
-//     <div>
-//       <h1>Dashboard</h1>
-//       <CreateTask onTaskCreated={() => window.location.reload()} />
-//       <TaskList />
-//       <CalendarView />
-//     </div>
-//   );
-// };
-
-// export default Dashboard;
 import React from 'react';
 import CreateTask from '../components/Task/CreateTask';
 import TaskList from '../components/Task/TaskList';
 import CalendarView from '../components/Task/CalendarView';
-import './Dashboard.css'; // 👈 Import CSS
+import './Dashboard.css'; 
 
 const Dashboard = () => {
   return (
